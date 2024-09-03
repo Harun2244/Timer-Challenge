aea53e69cfe6de8e7a6806085bf95d5ed4b2c462
harun_d <harun.duranovic@dooherceg.ba>
2024-09-03 19:02:33 +0200

    first commit

---
+++ 01-starting-project/src/components/TimerChallenge.jsx
@@ -0,0 +1,56 @@
+import { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
+
+export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+  const dialog = useRef();
+
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
+
+  function handleStart() {
+    timer.current = setInterval(() => {
+      setTimeRemaining((previousTimeRemaining) => previousTimeRemaining - 10);
+    }, 10);
+  }
+
+  function handleStop() {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  return (
+    <>
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p>
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} challenge
+          </button>
+        </p>
+        <p className={timerIsActive ? "active" : undefined}>
+          Timer {timerIsActive ? "is running" : "inactive"}
+        </p>
+      </section>
+    </>
+  );
+}
